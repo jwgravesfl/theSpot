2c044ef93695b9306c3b84d7d328e18a50f5dcba
J. Graves <jwgravesfl@gmail.com>
2018-09-06 02:08:48 -0600

    add notes, display notes both work and persist

--- src/components/BarNotesPopover.jsx
+++ src/components/BarNotesPopover.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap'
+import { Form, Button, Popover, PopoverHeader, Modal, ModalHeader, ModalBody, Label, Input, ModalFooter } from 'reactstrap'
 
 import firebase from '../firebase/firebase'
 
@@ -9,14 +9,50 @@ export default class BarNotesPopover extends Component {
     super(props)
     this.toggle = this.toggle.bind(this)
     this.state = {
+      spotID: '',
+      note: '',
+      date: '',
       popoverOpen: false,
+      modal: false,
       notes: []
     }
-  }
+    this.toggle = this.toggle.bind(this)
+    this.toggleModal = this.toggleModal.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e) {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
 
   toggle() {
     this.setState({
       popoverOpen: !this.state.popoverOpen
+    })
+  }
+
+  toggleModal() {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const newBarNotesRef = firebase.database().ref('barNotes')
+    const barNote = {
+      spotID: this.props.spotID,
+      date: this.state.date,
+      note: this.state.note,
+    }
+    newBarNotesRef.push(barNote)
+    this.setState({
+      date: '',
+      note: '',
+      modal: false
     })
   }
 
@@ -48,6 +84,52 @@ export default class BarNotesPopover extends Component {
     const spotID = this.props.spotID
     return (
       <div>
+      <Button color="danger" onClick={this.toggleModal}>New Note</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggleModal} className="">
+        <Form onSubmit={this.handleSubmit}>
+          <ModalHeader toggle={this.toggleModal}>Add Note</ModalHeader>
+          <ModalBody>
+          
+            <div className="row">
+              <div className="col">
+              <Label 
+                  for="date"
+                  className="asLabel"
+                >Date</Label>
+                <Input
+                  type="date" 
+                  name="date" 
+                  placeholder="date"
+                  id="date" 
+                  onChange={this.handleChange} 
+                  value={this.state.date}
+                  className="asInput"
+                />  
+              </div>
+              <div className="col">
+              <Label 
+                  for="note"
+                  className="asLabel"
+                >Note</Label>
+                <Input
+                  type="textarea" 
+                  name="note" 
+                  placeholder="Add Note"
+                  id="note" 
+                  onChange={this.handleChange} 
+                  value={this.state.note}
+                  className="asInput"
+                />
+              </div>
+            </div>
+            
+          </ModalBody>
+          <ModalFooter>
+          <Button type="submit" color="primary">Add Note</Button>
+            <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+          </ModalFooter>
+          </Form>
+        </Modal>
         <Button id={"Popover1" + spotID} onClick={this.toggle}>
           Notes
         </Button>
